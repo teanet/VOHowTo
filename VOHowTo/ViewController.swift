603d904c4d2af3912a825f8738d99433f39ef25b
e.tyutyuev <e.tyutyuev@2gis.ru>
2024-06-15 11:41:20 +0700

    Initial Commit

---
+++ VOHowTo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VOHowTo
+//
+//  Created by teanet on 15.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
